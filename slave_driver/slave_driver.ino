ba08b4ce97cb506d0241e75d69ecb79adffdcff3
Real Nguyen <real.nguyen@hotmail.com>
2018-12-05 19:14:16 -0500

    Motor control now works with phase correct PWM

--- slave_driver/slave_driver.ino
+++ slave_driver/slave_driver.ino
@@ -3,9 +3,15 @@
 
 void init_motors(void);
 void init_sensor(void);
+void brake(void);
+void drive_forwards(void);
+void drive_left(void);
+void drive_right(void);
+void drive_backwards(void);
 
 int main(void)
 {
+    //Serial.begin(9600);
     init_motors();
     //init_sensor();
     
@@ -18,49 +24,100 @@ int main(void)
 
 void init_motors(void)
 {
-    //Setup outputs
-    DDRD = (1 << PD5) | (1 << PD6);                             //D5 D6
-    DDRC = (1 << PC2) | (1 << PC3) | (1 << PC4) | (1 << PC5);   //A2 A3 A4 A5
-    // configure non-inverted fast PWM
-    TCCR0A = (1 << WGM00) | (1 << WGM01) | (1 << COM0B1) | (1 << COM0A1);
-    // configure timer and prescaler (no prescaler)
-    TCCR0B = (1 << CS00);
-    
-    OCR0A = 255;
-    OCR0B = 255;
+    // Setup outputs
+    // PWM pins D9 D10; D9 = left motors, D10 = right motors
+    DDRB = (1 << PB1) | (1 << PB2);
+    // GPIO pins; A2 A3 = left motors, A4 A5 = right motors
+    DDRC = (1 << PC2) | (1 << PC3) | (1 << PC4) | (1 << PC5);
+
+    // Phase correct PWM
+    TCCR1A = (1 << WGM13) | (1 << WGM11) | (1 << WGM10) | (1 << COM1A1) | (1 << COM1B1) | (1 << COM1A0) | (1 << COM1B0);
+    // No prescaler
+    TCCR1B = (1 << CS00);
+
+    OCR1A = 128;
+    OCR1B = 128;
 }
 
 void init_sensor(void)
 {
-  // configure A0 as input
-  DDRC = (0 << PC0);
+    // configure A0 as input
+    DDRC = (0 << PC0);
+    
+    // configure prescaler (/2)
+    ADCSRA = (1 << ADPS2);
+    // enable ADC
+    ADCSRA |= (1 << ADEN);
   
-  // configure prescaler (/2)
-  ADCSRA = (1 << ADPS2);// | (0 << ADPS1) | (0 << ADPS0);
-  // enable ADC
-  ADCSRA |= (1 << ADEN);
-
-  // use AVcc as the voltage reference w/ external capacitor on AREF pin
-  ADMUX |= (1 << REFS0);
+    // use AVcc as the voltage reference w/ external capacitor on AREF pin
+    ADMUX |= (1 << REFS0);
 }
 
 //test code; rename and create signature
 void test_motors(void)
-{
-      //TODO: going from forwards to backwards very iffy. Try braking with PWM first?
-  
-      // forwards 
-      PORTC = (0 << PC5) | (1 << PC4) | (1 << PC3) | (0 << PC2);
-      _delay_ms(2000);
+{  
+    drive_forwards();
+    _delay_ms(5000);
+
+    brake();
+    _delay_ms(5000);
+
+    drive_right();
+    _delay_ms(5000);
+
+    brake();
+    _delay_ms(5000);
     
-      // backwards
-      PORTC = (1 << PC5) | (0 << PC4) | (0 << PC3) | (1 << PC2);
-      _delay_ms(2000);
+    drive_left();
+    _delay_ms(5000);
+
+    brake();
+    _delay_ms(5000);
+
+    drive_backwards();
+    _delay_ms(5000);
+
+    brake();
+    _delay_ms(5000);
 }
 
 
 //test code; rename and create signature
 void test_sensor(void)
 {
+    ADCSRA |= (1 << ADSC); //Start Conversion
+    while (!(ADCSRA & (1 << ADIF))); //Wait for ADIF to go to 0, indicating conversion complete.
+    ADCSRA |= (1 << ADIF);
 
+    // clear ADIF manually
+    ADCSRA |= (1 << ADIF);
+    
+    uint8_t value = ADC / 2;
+    //Serial.println(value);
+    _delay_ms(100);
+}
+
+void brake(void)
+{
+    PORTC = 0;
+}
+
+void drive_forwards(void)
+{
+    PORTC = (1 << PC4) | (1 << PC3);
+}
+
+void drive_left(void)
+{
+    PORTC = (1 << PC4); 
+}
+
+void drive_right(void)
+{
+    PORTC = (1 << PC3);
+}
+
+void drive_backwards(void)
+{
+    PORTC = (1 << PC5) | (1 << PC2);
 }
